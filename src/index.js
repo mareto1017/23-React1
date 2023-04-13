def72c2ef672282bdaeccce31bd37f2c2cd1d662
mareto1017 <oktobroesperi@naver.com>
2023-04-14 02:15:28 +0900

    0413 7주차

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Accommodate from './chapter_07/Accommodate';
 
 import Library from './chapter_03/Library';
 import Clock from './chapter_04/Clock';
@@ -13,7 +14,7 @@ import NotificationList from './chapter_06/NotificationList';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NotificationList />
+    <Accommodate />
   </React.StrictMode>
 );
 
